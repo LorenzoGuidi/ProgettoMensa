6ae92d557c7d05cbb8b0c526d4415794089a67d5
vermenuso <simone.marinolab@gmail.com>
2023-06-26 19:54:05 +0200

    first commit

---
+++ src/main/java/it/AsiloMensa/MensaAsilo/MensaAsiloApplication.java
@@ -0,0 +1,13 @@
+package it.AsiloMensa.MensaAsilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensaAsiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensaAsiloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/AsiloMensa/MensaAsilo/MensaAsiloApplicationTests.java
@@ -0,0 +1,13 @@
+package it.AsiloMensa.MensaAsilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensaAsiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
